d1fc8be3804d459a01ff3093b9696b42034981ab
himo77 <56316743+himo77@users.noreply.github.com>
2020-02-06 13:18:23 -0600

    Add project files.

---
+++ FullTime-PartTime/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FullTime_PartTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnWhatStatus = new System.Windows.Forms.Button();
+            this.numCredits = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numCredits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of credits";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(77, 290);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(291, 78);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status shown here";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // btnWhatStatus
+            // 
+            this.btnWhatStatus.Location = new System.Drawing.Point(156, 198);
+            this.btnWhatStatus.Name = "btnWhatStatus";
+            this.btnWhatStatus.Size = new System.Drawing.Size(266, 57);
+            this.btnWhatStatus.TabIndex = 3;
+            this.btnWhatStatus.Text = "What\'s my status?";
+            this.btnWhatStatus.UseVisualStyleBackColor = true;
+            this.btnWhatStatus.Click += new System.EventHandler(this.btnWhatStatus_Click);
+            // 
+            // numCredits
+            // 
+            this.numCredits.Location = new System.Drawing.Point(597, 88);
+            this.numCredits.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numCredits.Name = "numCredits";
+            this.numCredits.Size = new System.Drawing.Size(120, 31);
+            this.numCredits.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numCredits);
+            this.Controls.Add(this.btnWhatStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Full time, half time?";
+            ((System.ComponentModel.ISupportInitialize)(this.numCredits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnWhatStatus;
+        private System.Windows.Forms.NumericUpDown numCredits;
+    }
+}
+
---
+++ FullTime-PartTime/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullTime_PartTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWhatStatus_Click(object sender, EventArgs e)
+        {
+            decimal credits = numCredits.Value;
+
+
+            if (credits >= 12)
+            {
+                lblStatus.Text = "Full time";
+            }
+            else if (credits >= 6)
+            {
+                lblStatus.Text = "Half time";
+            }
+            else if (credits >= 0)
+
+            {
+                lblStatus.Text = "Less than half time";
+                
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
